49a0bcbc1400f37155d0fe024b3a309d96266037
moneyisgood <1156390711@qq.com>
2020-08-09 11:51:47 +0800

    添加注释

--- src/com/demo/day0001/Solution.java
+++ src/com/demo/day0001/Solution.java
@@ -2,6 +2,29 @@ package com.demo.day0001;
 
 import java.util.Arrays;
 
+/**
+ * 66. 加一
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ *
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ *
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3]
+ * 输出: [1,2,4]
+ * 解释: 输入数组表示数字 123。
+ * 示例 2:
+ *
+ * 输入: [4,3,2,1]
+ * 输出: [4,3,2,2]
+ * 解释: 输入数组表示数字 4321。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/plus-one
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Solution.main");
@@ -10,6 +33,11 @@ public class Solution {
         System.out.println(Arrays.toString(plusOne2(sd)));
     }
 
+    /**
+     * 方法1
+     * @param digits
+     * @return
+     */
     public static int[] plusOne(int[] digits) {
         int[] ret = digits;
         int s = 0;//上一循环取商的数字，如果不为0要加到当前数字后再计算
@@ -38,6 +66,11 @@ public class Solution {
         return digits;
     }
 
+    /**
+     * 方法2
+     * @param digits
+     * @return
+     */
     public static int[] plusOne2(int[] digits) {
         // 从末尾开始循环
         for (int i = digits.length - 1; i >= 0; i--) {
